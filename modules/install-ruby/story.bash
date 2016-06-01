edad62caa97e9ba095ab477aa541bee05d757ba9
Alexey Melezhik <melezhik@gmail.com>
2016-06-01 14:37:12 +0000

    ruby-install

--- modules/install-ruby/story.bash
+++ modules/install-ruby/story.bash
@@ -1,2 +1,16 @@
+yum -y install which
+
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - || exit 1
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby || exit 1
+
+source /usr/local/rvm/scripts/rvm
+
+gem install bundler --no-ri --no-rdoc
+
+echo ruby version: $(ruby --version)
+
+bundler --version
+
 echo install-ruby-ok
 
