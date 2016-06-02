9dcefa6b76cb4caf8b0cfb1fb88f976957135911
Alexey Melezhik <melezhik@gmail.com>
2016-06-02 10:28:57 +0000

    bundler install fix

--- modules/install-app/story.bash
+++ modules/install-app/story.bash
@@ -7,10 +7,10 @@ echo downloading $source_url  ...
 sudo -u $user_id -E bash --login -c "curl -f -o ~/app.tar.gz $source_url -s" || exit 1
 
 echo unpacking app ...
-sudo -u $user_id -E bash --login -c "tar -xzf app.tar.gz" || exit 1
+sudo -u $user_id -E bash --login -c "cd ~/ && tar -xzf app.tar.gz" || exit 1
 
 echo installing dependencies via bundler
-sudo -u $user_id -E bash --login -c "cd ~/app && bundle install --quiet " || exit 1
+sudo -u $user_id -E bash --login -c "cd ~/app && bundle install --path vendor/bundle --quiet " || exit 1
 
 echo install-app-ok
 
