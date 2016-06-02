8619923e37610544f5f50325382a7c325cf204ac
Alexey Melezhik <melezhik@gmail.com>
2016-06-02 10:22:40 +0000

    some fixes

--- modules/install-app/story.bash
+++ modules/install-app/story.bash
@@ -1,14 +1,16 @@
 user_id=$(config user_name)
 source_url=$(config source_url)
 
+yum -y -q install sudo
+
 echo downloading $source_url  ...
 sudo -u $user_id -E bash --login -c "curl -f -o ~/app.tar.gz $source_url -s" || exit 1
 
-echo unpacking /tmp/service.zip  into $service_path
+echo unpacking app ...
 sudo -u $user_id -E bash --login -c "tar -xzf app.tar.gz" || exit 1
 
 echo installing dependencies via bundler
-sudo -u $user_id -E bash --login -c "cd app bundle install --quiet " || exit 1
+sudo -u $user_id -E bash --login -c "cd ~/app && bundle install --quiet " || exit 1
 
 echo install-app-ok
 
