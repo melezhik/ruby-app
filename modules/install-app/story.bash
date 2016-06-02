a33ad37dee48aee1f9136bb88169e56bc03d77dc
Alexey Melezhik <melezhik@gmail.com>
2016-06-02 10:42:13 +0000

    all done

--- modules/install-app/story.bash
+++ modules/install-app/story.bash
@@ -12,5 +12,7 @@ sudo -u $user_id -E bash --login -c "cd ~/ && tar -xzf app.tar.gz" || exit 1
 echo installing dependencies via bundler
 sudo -u $user_id -E bash --login -c "cd ~/app && bundle install --path vendor/bundle --quiet " || exit 1
 
+sudo -u $user_id -E bash --login -c "cd ~/app && bundle exec ruby hello.rb " || exit 1
+
 echo install-app-ok
 
